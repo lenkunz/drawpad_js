2b083a8e07dded80d03ec2a9d08d9c4a2f0099e6
Rapeapach Suwasri <kagaminelenkyun@gmail.com>
2014-08-08 23:12:58 +0700

    Some config change.

---
+++ dist/drawpad.js
@@ -0,0 +1,6 @@
+/*! Drawpad API - v0.1.0 - 2014-08-08
+* https://github.com/lenkyun/drawpad_js
+* Copyright (c) 2014 Rapeapach Suwasri; Licensed CC0, 1.0 */
+(function($) {
+	$('aa').css('b');
+}(jQuery));
---
+++ src/drawpad.js
@@ -0,0 +1,13 @@
+/*
+ * drawpad
+ * https://github.com/lenkyun/drawpad_js
+ *
+ * Copyright (c) 2014 Rapeapach Suwasri
+ * Licensed under the CC0, 1.0 licenses.
+ */
+
+(function($) {
+	define(["./pad/core", "/controller/core"],function(pad, core){
+	
+	});
+}(jQuery));
